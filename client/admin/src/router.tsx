71326c681ac39e1b019b366ce5b2ebac3c3313d6
Javier Caballero <javier.caballero@compass.com>
2025-04-13 17:05:15 -0400

    Add dashboard page and implement access control for categories

---
+++ client/admin/src/router.tsx
@@ -0,0 +1,58 @@
+import {Outlet, Route, Routes} from "react-router";
+import {Authenticated, CanAccess, usePermissions} from "@refinedev/core";
+import {CatchAllNavigate, NavigateToResource} from "@refinedev/react-router";
+import {ErrorComponent, ThemedLayoutV2, ThemedSiderV2} from "@refinedev/antd";
+import {Header} from "./components";
+import {CategoryList} from "./pages/categories";
+import Dashboard from "./pages/dashboard";
+import {Login} from "./pages/login";
+import React from "react";
+
+const AppRouter = () => {
+  const {data: permissionsData} = usePermissions();
+
+  return <Routes>
+    <Route
+      element={
+        <Authenticated
+          key="authenticated-inner"
+          fallback={<CatchAllNavigate to="/login"/>}
+        >
+          <ThemedLayoutV2
+            Header={Header}
+            Sider={() =>
+              <ThemedSiderV2 fixed render={({items,}) => <>{items}</>}/>
+            }
+          >
+            <CanAccess fallback={<Dashboard/>}><Outlet/></CanAccess>
+          </ThemedLayoutV2>
+        </Authenticated>
+      }
+    >
+      <Route path=""
+             element={<NavigateToResource resource="dashboard"/>}
+      />
+      <Route path="/categories">
+        <Route index element={<CategoryList/>}/>
+      </Route>
+      <Route path="/">
+        <Route index element={<Dashboard/>}/>
+      </Route>
+      <Route path="*" element={<ErrorComponent/>}/>
+    </Route>
+    <Route
+      element={
+        <Authenticated
+          key="authenticated-outer"
+          fallback={<Outlet/>}
+        >
+          <NavigateToResource/>
+        </Authenticated>
+      }
+    >
+      <Route path="/login" element={<Login/>}/>
+    </Route>
+  </Routes>
+};
+
+export default AppRouter;
