eec94501932ee81e9c3670c4aa9f49ea1982e881
Javier Caballero <javier.caballero@compass.com>
2025-04-13 15:24:03 -0400

    Refactor header and category list components; add modals for create and edit actions

--- client/admin/src/components/header/index.tsx
+++ client/admin/src/components/header/index.tsx
@@ -1,16 +1,17 @@
 import type {RefineThemedLayoutV2HeaderProps} from "@refinedev/antd";
-import {useGetIdentity} from "@refinedev/core";
-import {Avatar, Layout as AntdLayout, Space, Switch, theme, Typography,} from "antd";
+import {useGetIdentity, useLogout, useTranslate} from "@refinedev/core";
+import {Avatar, Dropdown, Layout as AntdLayout, MenuProps, Space, Switch, theme, Typography,} from "antd";
 import React, {useContext} from "react";
 import {ColorModeContext} from "../../contexts/color-mode";
+import {LogoutOutlined} from "@ant-design/icons";
 
-const {Text} = Typography;
 const {useToken} = theme;
 
-type IUser = {
+export type IUser = {
   id: number;
   name: string;
   avatar: string;
+  roles: string[];
 };
 
 export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
@@ -19,6 +20,23 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
   const {token} = useToken();
   const {data: user} = useGetIdentity<IUser>();
   const {mode, setMode} = useContext(ColorModeContext);
+  const translate = useTranslate();
+  const {mutate: mutateLogout} = useLogout();
+
+  const items: MenuProps['items'] = [
+    {
+      label: (
+        <div
+          key="logout"
+          onClick={() => mutateLogout()}
+        >
+          {translate("buttons.logout", "Logout")}
+        </div>
+      ),
+      icon: <LogoutOutlined/>,
+      key: '0',
+    }
+  ];
 
   const headerStyles: React.CSSProperties = {
     backgroundColor: token.colorBgElevated,
@@ -44,10 +62,13 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
           onChange={() => setMode(mode === "light" ? "dark" : "light")}
           defaultChecked={mode === "dark"}
         />
-        <Space style={{marginLeft: "8px"}} size="middle">
-          {user?.name && <Text strong>{user.name}</Text>}
-          {user?.avatar && <Avatar src={user?.avatar} alt={user?.name}/>}
-        </Space>
+
+        <Dropdown menu={{items}} trigger={['click']} arrow placement="bottomRight">
+          <Space style={{marginLeft: "8px", cursor: "pointer"}} size="middle">
+            {user?.name && <Typography.Text strong>{user.name}</Typography.Text>}
+            {user?.avatar && <Avatar src={user?.avatar} alt={user?.name}/>}
+          </Space>
+        </Dropdown>
       </Space>
     </AntdLayout.Header>
   );
--- client/admin/src/pages/categories/list.tsx
+++ client/admin/src/pages/categories/list.tsx
@@ -1,29 +1,192 @@
-import {DeleteButton, EditButton, List, ShowButton, useTable,} from "@refinedev/antd";
-import type {BaseRecord} from "@refinedev/core";
-import {Space, Table} from "antd";
+import {DeleteButton, EditButton, List, ShowButton, useModalForm, useTable,} from "@refinedev/antd";
+import {type BaseRecord, useShow} from "@refinedev/core";
+import {Form, Input, Modal, Space, Spin, Table, Typography} from "antd";
+import {useState} from "react";
+
+const {Title, Text} = Typography;
+
+
+interface ICategory {
+  id: number;
+  name: string;
+}
+
+const modalFormStyles = {
+  wrap: {
+    maxWidth: "600px",
+    width: "100%",
+    margin: "0 auto",
+  },
+  content: {
+    padding: 0,
+    maxWidth: "600px",
+    width: "100%",
+    margin: "0 auto",
+  },
+  header: {
+    margin: 0,
+    minHeight: '60px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '8px 16px'
+  },
+  body: {
+    padding: "16px",
+    margin: 0,
+    borderTop: "1px solid #f0f0f0",
+    borderBottom: "1px solid #f0f0f0",
+  },
+  footer: {
+    padding: '8px 16px'
+  },
+};
+
+const modalShowStyles = {
+  header: {
+    margin: 0,
+    minHeight: '60px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '8px 16px'
+  },
+  body: {
+    padding: "16px",
+    borderTop: "1px solid #f0f0f0",
+  },
+  footer: {
+    padding: 0,
+  },
+  content: {
+    padding: 0,
+    maxWidth: "600px",
+    margin: "0 auto",
+  },
+};
 
 export const CategoryList = () => {
-  const {tableProps} = useTable({
+  const {tableProps} = useTable<ICategory>({
     syncWithLocation: true,
   });
+  // Create Modal
+  const {
+    modalProps: createModalProps,
+    formProps: createFormProps,
+    show: createModalShow,
+    formLoading: createFormLoading,
+  } = useModalForm<ICategory>({
+    action: "create",
+    syncWithLocation: true,
+    warnWhenUnsavedChanges: true,
+  });
+
+  // Edit Modal
+  const {
+    modalProps: editModalProps,
+    formProps: editFormProps,
+    show: editModalShow,
+    formLoading: editFormLoading,
+  } = useModalForm<ICategory>({
+    action: "edit",
+    syncWithLocation: true,
+    warnWhenUnsavedChanges: true,
+  });
+
+  // Show Modal
+  const [visibleShowModal, setVisibleShowModal] = useState<boolean>(false);
+
+  const {query: queryResult, setShowId} = useShow<ICategory>();
+
+  const {data: showQueryResult} = queryResult;
+  const record = showQueryResult?.data;
 
   return (
-    <List>
-      <Table {...tableProps} rowKey="id">
-        <Table.Column dataIndex="id" title={"#"}/>
-        <Table.Column dataIndex="name" title={"Name"}/>
-        <Table.Column
-          title={"Actions"}
-          dataIndex="actions"
-          render={(_, record: BaseRecord) => (
-            <Space>
-              <EditButton hideText size="small" recordItemId={record.id}/>
-              <ShowButton hideText size="small" recordItemId={record.id}/>
-              <DeleteButton hideText size="small" recordItemId={record.id}/>
-            </Space>
-          )}
-        />
-      </Table>
-    </List>
+    <>
+      <List
+        createButtonProps={{
+          onClick: () => {
+            createModalShow();
+          },
+        }}
+      >
+        <Table {...tableProps} rowKey="id">
+          <Table.Column dataIndex="id" title={"#"}/>
+          <Table.Column dataIndex="name" title={"Name"}/>
+          <Table.Column
+            title={"Actions"}
+            width={100}
+            dataIndex="actions"
+            render={(_, record: BaseRecord) => (
+              <Space>
+                <EditButton
+                  hideText
+                  size="small"
+                  recordItemId={record.id}
+                  onClick={() => editModalShow(record.id)}
+                />
+                <ShowButton
+                  hideText
+                  size="small"
+                  recordItemId={record.id}
+                  onClick={() => {
+                    setShowId(record.id);
+                    setVisibleShowModal(true);
+                  }}
+                />
+                <DeleteButton
+                  hideText
+                  size="small"
+                  recordItemId={record.id}
+                />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+      <Modal {...createModalProps} styles={modalFormStyles}>
+        <Spin spinning={createFormLoading}>
+          <Form {...createFormProps}>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input/>
+            </Form.Item>
+          </Form>
+        </Spin>
+      </Modal>
+      <Modal {...editModalProps} styles={modalFormStyles}>
+        <Spin spinning={editFormLoading}>
+          <Form {...editFormProps} layout="vertical">
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input/>
+            </Form.Item>
+          </Form>
+        </Spin>
+      </Modal>
+      <Modal
+        open={visibleShowModal}
+        footer={false}
+        onCancel={() => setVisibleShowModal(false)}
+        title="Show post"
+        styles={modalShowStyles}>
+        <Title level={5}>Id</Title>
+        <Text>{record?.id}</Text>
+        <Title level={5}>Name</Title>
+        <Text>{record?.name}</Text>
+      </Modal>
+    </>
   );
 };
