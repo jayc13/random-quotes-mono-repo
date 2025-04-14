e0febef73e3ea7d62318b517119b26971b3ad95c
Javier Caballero <javier.caballero@compass.com>
2025-04-14 10:35:32 -0400

    Configure Vite build options to set chunk size warning limit and define manual chunks for optimized code splitting

--- client/admin/vite.config.ts
+++ client/admin/vite.config.ts
@@ -3,4 +3,21 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800, // Set the limit to 800 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          antdLib: ['antd/lib'],
+          antdEs: ['antd/es'],
+          refinedevCore: ['@refinedev/core'],
+          refinedevAntd: ['@refinedev/antd'],
+          quotesPage: ['src/pages/quotes/list.tsx'], // Quotes page
+          indexCategoriesPage: ['src/pages/categories/list.tsx'], // Categories List page
+          showCategoriesPage: ['src/pages/categories/show.tsx'], // Categories Show page
+        },
+      },
+    },
+  },
 });
