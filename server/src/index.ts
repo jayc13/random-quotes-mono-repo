346ce2252e62f9b17b173ae479136845950167c9
Javier Caballero <javier.caballero@compass.com>
2025-04-16 09:32:29 -0400

    Remove AUTH0_CLIENT_SECRET from environment variables and update issuer format in JWT parsing

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,6 @@ export interface Env {
 	// If you set another name in the Wrangler config file for the value for 'binding',
 	// replace "DB" with the variable name you defined.
 	DB: D1Database;
-	AUTH0_CLIENT_SECRET: string;
 	AUTH0_DOMAIN: string;
 	AUTH0_CLIENT_ID: string;
 }
