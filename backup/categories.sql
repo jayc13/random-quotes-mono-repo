f6db664946e2e44bf643e1404cc3036ac47d6a7b
Javier Caballero <javier.caballero@compass.com>
2025-04-19 17:07:19 -0400

    chore: initialize categories table with predefined values

---
+++ backup/categories.sql
@@ -0,0 +1,16 @@
+DELETE FROM Categories;
+
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (1, 'Happiness');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (2, 'Motivation');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (3, 'Learning');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (4, 'Wisdom');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (5, 'Mindfulness');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (6, 'Philosophy');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (7, 'Friendship');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (8, 'Courage');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (9, 'Love');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (10, 'Humor');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (11, 'Leadership');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (12, 'Life');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (13, 'Inspiration');
+INSERT INTO Categories (CategoryId, CategoryName) VALUES (14, 'Success');
