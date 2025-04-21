043c6ba08da01fceccea8d168866123c65cfc7e6
Javier Caballero <javier.caballero@compass.com>
2025-04-21 17:53:46 -0400

    feat: add Playwright tests for access control and user authentication

---
+++ test/e2e-tests/tests/admin/regular-user/access-control.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect, Page } from '@playwright/test';
+import { ADMIN_BASE_URL } from '../../../utils/config';
+import { loginAs } from '../../../commands/admin/login.command';
+
+test.describe.configure({ mode: 'serial' });
+
+let page: Page;
+
+test.describe('Regular users - Access Control', () => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await loginAs(page, 'REGULAR');
+  });
+  test('only the home page is accessible after login', async () => {
+    await onlyHomePageAccessible(page);
+  });
+  test('/categories path requires admin login', async () => {
+    await page.goto(`${ADMIN_BASE_URL}/categories`);
+    const mainContainer =  page.locator('header');
+    await mainContainer.waitFor({ state: 'visible' });
+    await onlyHomePageAccessible(page);
+  });
+  test('/quotes path requires admin login', async () => {
+    await page.goto(`${ADMIN_BASE_URL}/quotes`);
+    const mainContainer =  page.locator('header');
+    await mainContainer.waitFor({ state: 'visible' });
+    await onlyHomePageAccessible(page);
+  });
+});
+
+async function onlyHomePageAccessible(page: Page) {
+  const sideMenuOptions = await page.locator('aside ul li a').all();
+  expect(sideMenuOptions.length).toBe(1);
+  const homeOption = sideMenuOptions[0];
+  expect(await homeOption.innerText()).toBe('Home');
+}
