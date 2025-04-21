60637675a0c50d8623424b0db5b89c2fcdee7c77
Javier Caballero <javier.caballero@compass.com>
2025-04-21 18:38:52 -0400

    refactor: simplify Playwright configuration and adjust reporter settings

--- test/e2e-tests/playwright.config.ts
+++ test/e2e-tests/playwright.config.ts
@@ -15,15 +15,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['list', { printSteps: true }],
-    ['html']
-  ],
+  reporter: [['github'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: 'retain-on-failure',
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
