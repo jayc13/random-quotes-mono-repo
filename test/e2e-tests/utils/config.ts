5b84768ebff5e42dc94c042e4f3171be7b2839bd
Javier Caballero <javier.caballero@compass.com>
2025-04-23 18:53:57 -0400

    feat: Add environment variables for admin and consumer base URLs

--- test/e2e-tests/utils/config.ts
+++ test/e2e-tests/utils/config.ts
@@ -1,1 +1,3 @@
-export const ADMIN_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8787';
+export const ADMIN_BASE_URL = process.env.ADMIN_BASE_URL || 'http://localhost:3000';
+export const CONSUMER_BASE_URL = process.env.CONSUMER_BASE_URL || 'http://localhost:3000';
