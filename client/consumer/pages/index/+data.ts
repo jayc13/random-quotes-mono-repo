e7e2effe16707d11cb7fbf7196a223de322aea8d
Javier Caballero <javier.caballero@compass.com>
2025-04-23 15:57:22 -0400

    feat: Update BASE_DATA_API to use VITE_DATA_API for better environment variable management

--- client/consumer/pages/index/+data.ts
+++ client/consumer/pages/index/+data.ts
@@ -3,10 +3,7 @@ import { render } from "vike/abort";
 
 export type Data = Awaited<ReturnType<typeof data>>;
 
-// const BASE_DATA_API = process.env.DATA_API || '';
 const BASE_DATA_API = import.meta.env.VITE_DATA_API || '';
-
-console.log({BASE_DATA_API})
 
 export const data = async () => {
 	const response = await fetch(`${BASE_DATA_API}/random`);
