0ab1e1f3ef14ba73eac8b19881ac368ee7b65f47
Javier Caballero <javier.caballero@compass.com>
2025-04-23 16:02:54 -0400

    fix: Standardize string quotes for BASE_DATA_API in data.ts

--- client/consumer/pages/index/+data.ts
+++ client/consumer/pages/index/+data.ts
@@ -3,7 +3,7 @@ import { render } from "vike/abort";
 
 export type Data = Awaited<ReturnType<typeof data>>;
 
-const BASE_DATA_API = import.meta.env.VITE_DATA_API || '';
+const BASE_DATA_API = import.meta.env.VITE_DATA_API || "";
 
 export const data = async () => {
 	const response = await fetch(`${BASE_DATA_API}/random`);
