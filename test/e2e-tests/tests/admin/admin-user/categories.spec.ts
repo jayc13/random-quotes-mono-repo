b110282eb11efec9083b896b7cb35a7298701e10
Javier Caballero <javier.caballero@compass.com>
2025-04-24 12:00:24 -0400

    feat: Enhance category management tests with dynamic category handling and improved request validation

--- test/e2e-tests/tests/admin/admin-user/categories.spec.ts
+++ test/e2e-tests/tests/admin/admin-user/categories.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect, Page } from '@playwright/test';
 import { loginAs } from '../../../commands/admin/login.command';
-import { ADMIN_BASE_URL } from '../../../utils/config';
+import {ADMIN_BASE_URL, API_BASE_URL} from '../../../utils/config';
 
 test.describe.configure({ mode: 'serial' });
 
 let page: Page;
+let categoryId: string;
 
 test.describe('Admin Categories Page', () => {
   test.beforeAll(async ({ browser }) => {
@@ -23,44 +24,120 @@ test.describe('Admin Categories Page', () => {
   });
 
   test('should allow adding a new category', async () => {
+    const categoryName = `New Category ${Date.now()}`;
     // Click on "Add Category" button
     await page.click('button#add-category-btn');
 
     // Fill out the form
-    await page.fill('[data-testid="categoryName"] input', 'New Category');
+    await page.fill('[data-testid="create-category-modal"] [data-testid="categoryName"] input', categoryName);
+
+    const createCategoryRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories`) && request.method() === 'POST';
+    });
+
+    const getAllCategoriesRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories`) && request.method() === 'GET';
+    });
+
     await page.getByRole('button', { name: 'Save' }).click();
 
-    // Verify the new category is added
-    const newCategory = page.locator('#categories-table table tbody tr', {
-      hasText: 'New Category',
-    });
-    await expect(newCategory).toBeVisible();
+    // Wait for the request to complete
+    await Promise.all([
+      createCategoryRequest,
+      getAllCategoriesRequest,
+    ]);
+
+    const createCategoryResponse = await (await createCategoryRequest).response();
+
+    expect(createCategoryResponse.status()).toBe(201);
+    const createCategoryBody = await createCategoryResponse.json();
+
+    expect(createCategoryBody.name).toEqual(categoryName);
+    expect(createCategoryBody.id).toBeDefined();
+    categoryId = createCategoryBody.id;
+
+    // Check if the new category is displayed in the table
+
+    const categoryRow = await isCategoryByIdDisplayed(categoryId);
+    expect(categoryRow).toBeTruthy();
   });
 
   test('should allow editing a category', async () => {
-    // Click on the edit button for the first category
-    await page.click('#categories-table table tbody tr:first-child button.edit-category');
+    const newCategoryName = `Updated Category ${Date.now()}`;
+    const editButton = page.getByTestId(`edit-category-${categoryId}`);
+
+    await expect(editButton).toBeVisible();
+    await editButton.click();
 
     // Update the category name
-    await page.fill('[data-testid="categoryName"] input', 'Updated Category');
+    await page.fill('[data-testid="edit-category-modal"] [data-testid="categoryName"] input', newCategoryName);
+
+    const editCategoryRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories/${categoryId}`) && request.method() === 'PATCH';
+    });
+
+    const getAllCategoriesRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories`) && request.method() === 'GET';
+    });
+
     await page.getByRole('button', { name: 'Save' }).click();
 
-    // Verify the category name is updated
-    const updatedCategory = page.locator('#categories-table table tbody tr', {
-      hasText: 'Updated Category',
-    });
-    await expect(updatedCategory).toBeVisible();
+    // Wait for the request to complete
+    await Promise.all([
+      editCategoryRequest,
+      getAllCategoriesRequest,
+    ]);
+
+    const editCategoryResponse = await (await editCategoryRequest).response();
+
+    expect(editCategoryResponse.status()).toBe(200);
+    const editCategoryBody = await editCategoryResponse.json();
+
+    expect(editCategoryBody.name).toEqual(newCategoryName);
+    expect(editCategoryBody.id).toEqual(categoryId);
+
+    const categoryRow = await isCategoryByIdDisplayed(categoryId);
+    expect(categoryRow).toBeTruthy();
   });
 
   test('should allow deleting a category', async () => {
-    // Click on the delete button for the first category
-    await page.click('#categories-table table tbody tr:first-child button.delete-category');
+    const deleteButton = page.getByTestId(`delete-category-${categoryId}`);
+    await expect(deleteButton).toBeVisible();
+    await deleteButton.click();
+
+    const deleteCategoryRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories/${categoryId}`) && request.method() === 'DELETE';
+    });
+
+    const getAllCategoriesRequest = page.waitForRequest((request) => {
+      return request.url().includes(`${API_BASE_URL}/categories`) && request.method() === 'GET';
+    });
 
     // Confirm the deletion
-    await page.click('button#confirm-delete');
+    await page.locator('[role="tooltip"] button').filter({hasText: 'Delete'}).click();
 
-    // Verify the category is removed
-    const deletedCategory = page.locator('#categories-table table tbody tr:first-child');
-    await expect(deletedCategory).not.toHaveText('Deleted Category');
+    await Promise.all([
+      deleteCategoryRequest,
+      getAllCategoriesRequest,
+    ]);
+
+    const categoryRow = await isCategoryByIdDisplayed(categoryId);
+    expect(categoryRow).toBeFalsy();
   });
 });
+
+async function isCategoryByIdDisplayed(id: string): Promise<boolean> {
+  const categoryRow = page.locator(`#categories-table tr[data-row-key="${id}"]`);
+  const isRowVisible = await categoryRow.isVisible();
+  if (!isRowVisible) {
+    // Navigate to the next page
+    const nextPageButton = page.locator('li[title="Next Page"][aria-disabled="false"] button');
+    if (await nextPageButton.isVisible()) {
+      await nextPageButton.click();
+      return isCategoryByIdDisplayed(id);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
