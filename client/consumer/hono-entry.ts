28497c22c64bc60d30677582d3496d8d9f7c044a
Javier Caballero <javier.caballero@compass.com>
2025-04-25 12:08:05 -0400

    Feat: Add telefunc route handler to hono-entry

--- client/consumer/hono-entry.ts
+++ client/consumer/hono-entry.ts
@@ -1,8 +1,11 @@
 import { vikeHandler } from "./server/vike-handler";
 import { Hono } from "hono";
 import { createHandler } from "@universal-middleware/hono";
+import { telefuncHandler } from "./server/telefunc-handler";
 
 const app = new Hono();
+
+app.post("/_telefunc", createHandler(telefuncHandler)());
 
 /**
  * Vike route
