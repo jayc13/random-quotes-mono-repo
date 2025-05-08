3aa5cd72d71f2b190ae20b800aedadd1e27985b8
Javier Caballero <javier.caballero@compass.com>
2025-05-07 20:49:01 -0400

    feat: Add allowed origins and API key setup in CI configuration

---
+++ backup/api_tokens.sql
@@ -0,0 +1,3 @@
+DELETE FROM ApiTokens;
+
+INSERT INTO ApiTokens (TokenName, HashedToken, UserId) VALUES ('ConsumerApp', '3b3f1b4f022d46bb78d9cd19a39f547144eccdd60c73911a0327444751c38b9d', 'e2e-test-user');
