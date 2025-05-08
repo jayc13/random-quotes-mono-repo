4f48d684eae16f221961cbdfe8a2175d1e3503cc
Javier Caballero <javier.caballero@compass.com>
2025-05-07 21:23:20 -0400

    test: Add Vitest setup for access control middleware tests

--- server/test/middlewares/accessControl.middleware.spec.ts
+++ server/test/middlewares/accessControl.middleware.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { accessControlMiddleware } from "@/middlewares/accessControl.middleware";
 import { validateApiToken } from "@/services/api-token.service";
 
